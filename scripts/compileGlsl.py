4045aa2f0d2051dc90072212cda2fa616fb5228c
Surge <esfloyd9341@gmail.com>
2024-12-27 12:08:01 -0500

    Add directory support to glsl compiler script

--- scripts/compileGlsl.py
+++ scripts/compileGlsl.py
@@ -17,6 +17,7 @@ Usage:
     python compileGlsl.py [-o output] [-d] <input>
 """
 
+import dotenv
 import os
 
 FILE_EXT_KNOWN = [".vert", ".frag", ".comp", ".geom", ".tesc", ".tese", ".mesh", ".task", ".glsl"]
@@ -28,7 +29,7 @@ def verifyGlslang() -> bool:
     Returns:
         bool: True if both glslang and glslangValidator are on the system PATH, False otherwise.
     """
-    return os.system("glslangValidator --version") == 0
+    return os.system("glslangValidator --version") == 0 or (os.getenv("GLSLANG_PATH") is not None and os.system(f"{os.getenv('GLSLANG_PATH/glslangValidator')} --version") == 0)
 
 def compileGlsl(input:str, output:str=None) -> bool:
     """
@@ -59,10 +60,13 @@ def compileGlsl(input:str, output:str=None) -> bool:
 
 
 if __name__=="__main__":
+    dotenv.load_dotenv() # Load environment variables from .env file, if it exists.
+
     if not verifyGlslang():
         print("glslang and glslangValidator must be on the system PATH.")
         exit(1)
 
+    # Parse command line arguments
     import argparse
 
     parser = argparse.ArgumentParser(description="Compile GLSL shaders into a single header file.")
@@ -74,7 +78,18 @@ if __name__=="__main__":
 
     args = parser.parse_args()
 
-    # TODO: directory parsing
-
-    if not compileGlsl(args.input, args.output):
-        exit(1)
+    if args.directory:
+        if not args.recursive:
+            for file in os.listdir(args.input):
+                if os.path.splitext(file)[1] in FILE_EXT_KNOWN:
+                    if not compileGlsl(os.path.join(args.input, file)):
+                        exit(1)
+        else:
+            for root, _, files in os.walk(args.input):
+                for file in files:
+                    if os.path.splitext(file)[1] in FILE_EXT_KNOWN:
+                        if not compileGlsl(os.path.join(root, file)):
+                            exit(1)
+    else:
+        if not compileGlsl(args.input, args.output):
+            exit(1)
